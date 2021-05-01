f5c9994c252052db044ae59ba0488ecce26a9de4
fillipe-felix <felipesoares_1993@hotmail.com>
2021-05-01 16:58:36 -0300

    Iniciando o projeto

---
+++ src/main/java/com/api/dio/springbootjtw/CriandoApiGatewaysNoAmbienteAzureSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package com.api.dio.springbootjtw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriandoApiGatewaysNoAmbienteAzureSpringCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriandoApiGatewaysNoAmbienteAzureSpringCloudApplication.class, args);
+    }
+
+}
